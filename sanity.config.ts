3be285ab9777976742abb8d51077119aeee32a31
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-10 22:53:49 +1000

    create sanity schema and add to sanity config

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "sanity"
-import { deskTool } from "sanity/desk"
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import project from './sanity/schema/project-schema'
 
 const config = defineConfig({
   projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECTID}`,
-  dataset: "production",
-  title: "My Personal Website",
-  apiVersion: "2023-06-10",
-  basePath: "/admin",
-  plugins: [deskTool()]
+  dataset: 'production',
+  title: 'My Personal Website',
+  apiVersion: '2023-06-10',
+  basePath: '/admin',
+  plugins: [deskTool()],
+  schema: {types: [project]}
 })
 
 export default config
