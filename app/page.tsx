33bd23471674b7e16eb037cbee8b25bdc18d0922
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-11 14:05:54 +1000

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,17 @@ const Home = async () => {
     <div className='max-w-5xl mx-auto'>
       <h1 className='text-7xl font-extrabold pt-20'>Hello! I am 
         <span className='bg-gradient-to-r 
-          from-blue-400 
-          via-purple-500 
-          to-red-600 
+          from-blue-500 
+          to-red-400 
           bg-clip-text 
           text-transparent'> Irah
         </span>.
       </h1>
-      <p className='mt-3 text-xl text-gray-600'>I&apos;m a designer and a developer based in Sydney. Check out my projects.</p>
-      <h2 className='mt-24 font-bold text-gray-700 text-3xl'>My projects</h2>
+      <p className='mt-3 text-xl'>I&apos;m a designer and a developer based in Sydney. Check out some of my latest works.</p>
+      <h2 className='mt-24 font-bold text-3xl'>My projects</h2>
       <div className='mt-5 grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3 gap-8'>
         {projects.map((project) => (
-          <div key={project._id} className='border borger-gray-500 rounded-lg p-3'>
+          <div key={project._id} className='border borger-gray-200 rounded-lg p-3'>
             {project.image && (
               <Image 
                 src={project.image}
@@ -29,9 +28,8 @@ const Home = async () => {
               />
             )}
             <div className='font-extrabold bg-gradient-to-r 
-              from-blue-400 
-              via-purple-500 
-              to-red-600 
+              from-blue-500 
+              to-red-400 
               bg-clip-text 
               text-transparent'>{project.name}
             </div>
