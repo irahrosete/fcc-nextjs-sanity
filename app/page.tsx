1c9f3eb0d77788490ef54e82b689a420b24933f0
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-11 14:45:36 +1000

    add image config, update links and restyle

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from '@/sanity/utils'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Home = async () => {
   const projects = await getProjects()
@@ -17,23 +18,24 @@ const Home = async () => {
       <h2 className='mt-24 font-bold text-4xl'>My projects</h2>
       <div className='mt-5 grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3 gap-8'>
         {projects.map((project) => (
-          <div key={project._id} className='border borger-gray-200 rounded-lg p-3'>
+          <Link href={`/projects/${project.slug}`} key={project._id} 
+            className='border borger-gray-200 rounded-lg p-2 hover:scale-105 hover:border-gray-500 transition'>
             {project.image && (
               <Image 
                 src={project.image}
                 alt={project.name}
-                width={250}
-                height={100}
+                width={750}
+                height={300}
                 className='object-cover rounded-lg border border-gray-500'
               />
             )}
-            <div className='font-extrabold bg-gradient-to-r 
+            <div className='mt-2 font-extrabold bg-gradient-to-r 
               from-blue-500 
               to-red-400 
               bg-clip-text 
               text-transparent'>{project.name}
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
