292c447dff6791094b595cb63811be9d540cd6a7
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-12 14:45:48 +1000

    style individual project page

--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -7,7 +7,34 @@ type Props = {
 const Project = async ({ params }: Props) => {
   const slug = params.project
   const project = await getProject(slug)
-  return <div>{project.name}</div>
+  return ( 
+    <div className='max-w-3xl mx-auto py-20'>
+    <header className='flex items-center justify-between'>
+      <h1 className='bg-gradient-to-r 
+          from-blue-500 
+          to-red-400 
+          bg-clip-text 
+          text-transparent
+          text-5xl
+          drop-shadow
+          font-extrabold'>
+        {project.name}
+      </h1>
+      <a href={project.url} 
+        title='View Project' 
+        target='_blank' 
+        rel='noopener noreferrer'
+        className='bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-blue-100 hover:text-blue-400 transition'  
+      >
+        View Project
+      </a>
+    </header>
+
+    {/* {content goes here} */}
+    {/* {image goes here} */}
+    
+    </div>
+  )
 }
 
 export default Project
