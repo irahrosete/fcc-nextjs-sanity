eb7b38028c6f3f029922c34b911a70dfc2b9f791
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-12 15:42:11 +1000

    add navbar

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 const Home = async () => {
   const projects = await getProjects()
   return (
-    <div className='max-w-5xl mx-auto'>
-      <h1 className='text-9xl font-extrabold pt-20'>Hello! I am 
+    <div>
+      <h1 className='text-8xl font-extrabold'>Hello! I am 
         <span className='bg-gradient-to-r 
           from-blue-500 
           to-red-400 
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -10,7 +10,7 @@ const Project = async ({ params }: Props) => {
   const slug = params.project
   const project = await getProject(slug)
   return ( 
-    <div className='max-w-3xl mx-auto py-20'>
+    <div>
       <header className='flex items-center justify-between'>
         <h1 className='bg-gradient-to-r 
             from-blue-500 
@@ -26,7 +26,7 @@ const Project = async ({ params }: Props) => {
           title='View Project' 
           target='_blank' 
           rel='noopener noreferrer'
-          className='bg-gray-100 rounded-lg text-gray-500 font-bold py-3 px-4 whitespace-nowrap hover:bg-blue-100 hover:text-blue-400 transition'  
+          className='bg-gray-100 rounded-lg text-gray-800 font-bold py-3 px-4 whitespace-nowrap hover:bg-gray-800 hover:text-gray-100 transition'  
         >
           View Project
         </a>
