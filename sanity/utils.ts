d6b6abbcb33a25f57c8f607169b6b5e46f8e117b
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-12 14:10:15 +1000

    create individual project page

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -20,3 +20,24 @@ export const getProjects = async (): Promise<Project[]> => {
     }`
   )
 }
+
+export const getProject = async (slug: string): Promise<Project> => {
+  const client = createClient({
+    projectId: `${process.env.NEXT_PUBLIC_SANITY_PROJECTID}`,
+    dataset: 'production',
+    apiVersion: '2023-06-10'
+  })
+
+  return client.fetch(
+    groq`*[_type == 'project' && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      'slug': slug.current,
+      'image': image.asset -> url,
+      url,
+      content
+    }`,
+    { slug }
+  )
+}
